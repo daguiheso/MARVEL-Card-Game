9649af6e1bf51faeb137844b16f5010edcbc4ce9
Guillermo Hernandez <frontend@reddesignsystems.com>
2015-07-01 10:34:25 -0500

    Remplace functions to fat arrow => Ecmascript6

--- public/app.js
+++ public/app.js
@@ -5,7 +5,6 @@ var $ = window.jQuery;
 var key = 'apikey=f1527e7aae95ea7dc033527c850532d1';
 var url = 'http://gateway.marvel.com:80/v1/public/series?title=avengers&apikey=f1527e7aae95ea7dc033527c850532d1';
 Promise.resolve($.get(url)).then(function (results) {
-	// cuando acabe esta funcion
 	var characters = results.data.results[0].characters.items;
 	var promises = [];
 	for (var i in characters) {
